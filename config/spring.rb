39226627cdbc197c750a24deb1628d8164f3703b
Tanya Minchuk <zhoralux@gmail.com>
2019-06-02 23:57:02 +0300

    Update Gemfile, spring, seeds, yarn

--- config/spring.rb
+++ config/spring.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD
+
 %w[
   .ruby-version
   .rbenv-vars
   tmp/restart.txt
   tmp/caching-dev.txt
 ].each { |path| Spring.watch(path) }
-=======
-['.ruby-version', '.rbenv-vars', 'tmp/restart.txt', 'tmp/caching-dev.txt'].each { |path| Spring.watch(path) }
->>>>>>> master
