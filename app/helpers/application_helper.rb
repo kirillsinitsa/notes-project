f36164c5b6d191991fd9cbf68e74d15928139746
Helen <abedul@tut.by>
2019-06-12 21:03:20 +0300

    users and categories manipulations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
-# frozen_string_literal: true
-
 module ApplicationHelper
+  
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Notes"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
