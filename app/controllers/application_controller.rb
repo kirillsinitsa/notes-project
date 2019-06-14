86839856ce61e1a486eef02737df8c63a04ad924
Helen <abedul@tut.by>
2019-06-14 19:00:41 +0300

    errors correction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   # Confirms a logged-in user.
   def logged_in_user
     return if logged_in?
+
     store_location
     flash[:danger] = 'Please log in.'
     redirect_to login_url
